b494f09db35fe6cda67adcc8006f23d94fcc4ef0
Vasudev Purohit <53868512+vasudevpurohit@users.noreply.github.com>
2021-08-28 18:52:46 -0400

    Graphs added
    Added graphs for the following,
    
    -Linkage visualisations
    -theta1 v/s t
    -theta2 v/s t

--- Inverse Kinematics/HW0_2R_SerialChainRobot_Inv.m
+++ Inverse Kinematics/HW0_2R_SerialChainRobot_Inv.m
@@ -13,13 +13,19 @@ t = (0:1:360);
 xe = (1+(3*cosd(t)));
 ye = 3*sind(t);
 
+%calculating the joint angles using the reverse kinematic function
 [theta1,theta2] = RR_InvPosKin(L,xe,ye);
+
+%finding the coordinates for the first revolute joint
 x1 = L(1)*cosd(theta1);
 y1 = L(1)*sind(theta1);
 
+%checking if the end effector coordinates match the input
 Xe = (L(1)*cosd(theta1))+(L(2)*cosd(theta1+theta2));
 Ye = (L(1)*sind(theta1))+(L(2)*sind(theta1+theta2));
 
+%linkage plot for the entire motion of the end effector
+subplot(3,1,1);
 plot(Xe(1,:),Ye(1,:),'ro');
 axis equal
 hold on
@@ -34,3 +40,24 @@ legend('End Effector','Revolute Joint','Link 1','Link 2');
 xlabel('X (in cm)');
 ylabel('Y (in cm)');
 title('Inverse Kinematic Analysis');
+hold off
+
+%variation of uncrossed theta with the polar angle of the end effector
+subplot(3,1,2);
+plot(t,theta1(1,:),'b.');
+hold on
+plot(t,theta2(1,:),'r.');
+xlabel('Polar angle in deg');
+ylabel ('Theta in deg');
+title('Uncrossed configuration - Revolute angles');
+legend('Theta1','Theta2');
+
+%variation of crossed theta with the polar angle of the end effector
+subplot(3,1,3);
+plot(t,theta1(2,:),'b.');
+hold on
+plot(t,theta2(2,:),'r.');
+xlabel('Polar angle in deg');
+ylabel ('Theta in deg');
+title('Crossed configuration - Revolute angles');
+legend('Theta1','Theta2');
