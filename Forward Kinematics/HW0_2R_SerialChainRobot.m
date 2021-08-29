38584fa6ecd11e97f1467fc185c172844546965f
Vasudev Purohit <53868512+vasudevpurohit@users.noreply.github.com>
2021-08-28 22:06:43 -0400

    Added the final graphs to the forward kinema

--- Forward Kinematics/HW0_2R_SerialChainRobot.m
+++ Forward Kinematics/HW0_2R_SerialChainRobot.m
@@ -1,14 +1,14 @@
-% %%HW0 Serial Chain Robot - Forward Kinematics
+%% HW0 Serial Chain Robot - Forward Kinematics
 clear workspace
-% % Forward Kinematics
-% 
-% %defining the inputs
+
+%% Forward Kinematics
 L = [3,2]; %linkage lengths
-t = (0:1:360); 
+t = (0:1:360);
 
 [Xe,Ye,X1,Y1] = RR_ForwardPosKin(L,t);
 
 %plotting the results
+subplot(2,1,1)
 plot(t,Xe,'c.');
 hold on
 plot(t,Ye,'k.');
@@ -16,3 +16,10 @@ legend('End effector X-coordinate','End effector Y-coordinate');
 title('Forward Kinematics - 2R Serial chain');
 xlabel('t in deg');
 ylabel('End effector coordinate (cm)');
+
+subplot(2,1,2)
+plot(Xe,Ye,'r.');
+axis equal
+legend('End effector trajectory');
+xlabel('Xe (cm)');
+ylabel('Ye (cm)');
